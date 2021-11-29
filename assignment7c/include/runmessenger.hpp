e49342c6eb7ef73fe5b8212cf5f2a768e5a1c36c
Karen Koop Hogue <89603822+karenhogue@users.noreply.github.com>
2021-11-29 12:11:28 -0500

    Update geometry messenger

---
+++ assignment7c/include/runmessenger.hpp
@@ -0,0 +1,28 @@
+#ifndef RUN_MESSENGER_HPP
+#define RUN_MESSENGER_HPP
+#include "G4UImessenger.hh"
+#include "G4UIcmdWithABool.hh"
+#include "G4UIcmdWithAString.hh"
+
+namespace ne697 {
+  // Forward declaration, to resolve circular dependency with RunAction
+  // You still need to #include "runaction.hpp" in runmessenger.cpp
+  class RunAction;
+
+  // User-facing part of the UI: just for the user
+  class RunMessenger: public G4UImessenger {
+  public:
+    RunMessenger(RunAction* runaction);
+    ~RunMessenger();
+
+    void SetNewValue(G4UIcommand* cmd, G4String val) override final;
+
+  private:
+    RunAction* m_runAction;
+    G4UIdirectory* m_directory;
+    G4UIcmdWithABool* m_saveDataCmd;
+    G4UIcmdWithAString* m_savePathCmd;
+  };  
+}
+
+#endif
